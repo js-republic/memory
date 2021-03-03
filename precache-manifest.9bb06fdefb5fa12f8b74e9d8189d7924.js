a1a2bd948e5a6e831d51a1522753ee8357f61ea1
Nicolas Bienaime <nicolas.bienaime@ux-republic.com>
2021-03-03 16:10:18 +0100

    Updates

---
+++ precache-manifest.9bb06fdefb5fa12f8b74e9d8189d7924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5418b47ff93c0f9978544db5a880b3",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "1677f3b6e9daf368b8e1",
+    "url": "/memory/static/css/main.23f19505.chunk.css"
+  },
+  {
+    "revision": "c65d8c623d88c64d9ee8",
+    "url": "/memory/static/js/2.f925f4a3.chunk.js"
+  },
+  {
+    "revision": "33490309eac47256b827830c802fd4a5",
+    "url": "/memory/static/js/2.f925f4a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1677f3b6e9daf368b8e1",
+    "url": "/memory/static/js/main.d548df41.chunk.js"
+  },
+  {
+    "revision": "62e0eae13c9afaa66ed5",
+    "url": "/memory/static/js/runtime-main.c4984c0b.js"
+  }
+]);
